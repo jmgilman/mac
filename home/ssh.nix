02c88c4ed7245f2d4b4331e7c3aa1081519c432b
Joshua Gilman <joshuagilman@gmail.com>
2023-09-30 20:23:23 -0700

    refactor: simplify ssh config

--- home/ssh.nix
+++ home/ssh.nix
@@ -12,18 +12,6 @@
         hostname = "work.gilman.io";
         identityfile = "~/.ssh/id_ed25519_office";
       };
-
-      "*" = {
-        forwardagent = false;
-        compression = false;
-        serveraliveinterval = 0;
-        serveralivecountmax = 3;
-        hashknownhosts = false;
-        userknownhostsfile = "~/.ssh/known_hosts";
-        controlmaster = false;
-        controlpath = "~/.ssh/master-%r@%n:%p";
-        controlpersist = false;
-      };
     };
   };
   services.ssh-agent.enable = true;
