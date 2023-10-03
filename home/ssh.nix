900aaa089fb635027d9fffc6a6ec51124ce0924c
Joshua Gilman <joshuagilman@gmail.com>
2023-10-02 19:16:46 -0700

    chore: fix ssh key names

--- home/ssh.nix
+++ home/ssh.nix
@@ -2,15 +2,15 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      github = {
+      "github.com" = {
         user = "jmgilman";
         hostname = "github.com";
-        identityFile = "~/.ssh/id_ed25519_office";
+        identityFile = "~/.ssh/id_ed25519_studio";
       };
       work = {
         user = "josh";
         hostname = "work.gilman.io";
-        identityFile = "~/.ssh/id_ed25519_office";
+        identityFile = "~/.ssh/id_ed25519_studio";
       };
     };
   };
